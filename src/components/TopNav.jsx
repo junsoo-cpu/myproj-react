7c64fba091ea0396040162fc5c133c0555c2cd8b
JunSoo Lee <ljsay1113@naver.com>
2022-01-13 00:12:05 +0900

    TopNav css 설정

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,28 +2,42 @@ import { Link } from 'react-router-dom';
 
 function TopNav() {
   return (
-    <div class="app">
-      <ul className="flex gap-4 pb-3">
-        <li>
-          <MyLink to="/blog/">블로그</MyLink>
-        </li>
-        <li>
-          <MyLink to="/news/">뉴스룸</MyLink>
-        </li>
-        <li>
-          <MyLink to="accounts/login/">로그인</MyLink>
-        </li>
-        <li>
-          <MyLink to="/accounts/profile/">프로필</MyLink>
-        </li>
-        {/* <li>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <svg
+          class="fill-current h-8 w-8 mr-2"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">Top Navigation</span>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <ul className="flex gap-4 pb-3">
+            <li>
+              <MyLink to="/blog/">블로그</MyLink>
+            </li>
+            <li>
+              <MyLink to="/news/">뉴스룸</MyLink>
+            </li>
+            <li>
+              <MyLink to="accounts/login/">로그인</MyLink>
+            </li>
+            <li>
+              <MyLink to="/accounts/profile/">프로필</MyLink>
+            </li>
+            {/* <li>
           <MyLink to="/reviews/">리뷰</MyLink>
         </li>
         <li>
           <MyLink to="/examples/components/">컴포넌트 예시</MyLink>
         </li> */}
 
-        {/* <li>
+            {/* <li>
           <MyLink to="examples/clock/">시계</MyLink>
         </li>
         <li>
@@ -38,8 +52,10 @@ function TopNav() {
         <li>
           <MyLink to="examples/contextapisample2/">ContextApiSample2</MyLink>
         </li> */}
-      </ul>
-    </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
@@ -47,7 +63,7 @@ function MyLink({ to, children }) {
   return (
     <Link
       to={to}
-      className="pb-1 text-gray-500 hover:text-red-500 hover:border-red-500 border-b-4"
+      className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
     >
       {children}
     </Link>
