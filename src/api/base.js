81fa0e47db8d5f484432e8c1c89b47f2ae026acb
JunSoo Lee <ljsay1113@naver.com>
2022-01-12 15:39:45 +0900

    errorMessages 상탯값을 지원하는 useApiAxios 훅을 구현

--- src/api/base.js
+++ src/api/base.js
@@ -2,14 +2,36 @@ import Axios from 'axios';
 import { makeUseAxios } from 'axios-hooks';
 import { API_HOST } from 'Constants';
 import { useCallback, useEffect, useState } from 'react';
-
 const axiosInstance = Axios.create({
   baseURL: API_HOST,
 });
 
-const useApiAxios = makeUseAxios({
+const useAxios = makeUseAxios({
   axios: axiosInstance,
 });
+
+function useApiAxios(config, options) {
+  const [{ data, loading, error, response }, execute, manualCancel] = useAxios(
+    config,
+    options,
+  );
+
+  const [errorMessages, setErrorMessages] = useState({});
+
+  useEffect(() => {
+    if (error && error.response.status === 400) {
+      setErrorMessages(error.response.data);
+    } else {
+      setErrorMessages({});
+    }
+  }, [error]);
+
+  return [
+    { data, loading, error, response, errorMessages },
+    execute,
+    manualCancel,
+  ];
+}
 
 function useRequest(resourceUrl, initialState, manual = false) {
   const [data, setData] = useState(initialState);
@@ -50,5 +72,4 @@ function useRequest(resourceUrl, initialState, manual = false) {
   }, [manual]);
   return { data, loading, error, errorMessages, request };
 }
-
 export { axiosInstance, useApiAxios, useRequest };
--- src/components/news/ArticleForm.jsx
+++ src/components/news/ArticleForm.jsx
@@ -4,26 +4,28 @@ import H2 from 'components/H2';
 import LoadingIndicator from 'components/LoadingIndicator';
 import useFieldValues from 'hooks/useFieldValues';
 import { useApiAxios } from 'api/base';
-
 const INIT_FIELD_VALUES = { title: '', content: '' };
-
 // !articleId : 생성
 // articleId  : 수정
 
 function ArticleForm({ articleId, handleDidSave }) {
-  const [{ loading: saveLoading, error: saveError }, saveRequest] = useApiAxios(
+  const [
+    {
+      loading: saveLoading,
+      error: saveError,
+      errorMessages: saveErrorMessages,
+    },
+    saveRequest,
+  ] = useApiAxios(
     {
       url: '/news/api/articles/',
       method: 'POST',
     },
     { manual: true },
   );
-
   const { fieldValues, handleFieldChange } = useFieldValues(INIT_FIELD_VALUES);
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     saveRequest({
       data: fieldValues,
     }).then((response) => {
@@ -31,15 +33,12 @@ function ArticleForm({ articleId, handleDidSave }) {
       if (handleDidSave) handleDidSave(savedPost);
     });
   };
-
   return (
     <div>
       <H2>Article Form</H2>
-
       {saveLoading && <LoadingIndicator>저장 중 ...</LoadingIndicator>}
       {saveError &&
         `저장 중 에러가 발생했습니다. (${saveError.response.status} ${saveError.response.statusText})`}
-
       <form onSubmit={handleSubmit}>
         <div className="my-3">
           <input
@@ -49,6 +48,11 @@ function ArticleForm({ articleId, handleDidSave }) {
             type="text"
             className="p-1 bg-gray-100 w-full outline-none focus:border focus:border-gray-400 focus:border-dashed"
           />
+          {saveErrorMessages.title?.map((message, index) => (
+            <p key={index} className="text-xs text-red-400">
+              {message}
+            </p>
+          ))}
         </div>
 
         <div className="my-3">
@@ -58,15 +62,22 @@ function ArticleForm({ articleId, handleDidSave }) {
             onChange={handleFieldChange}
             className="p-1 bg-gray-100 w-full h-80 outline-none focus:border focus:border-gray-400 focus:border-dashed"
           />
+          {saveErrorMessages.content?.map((message, index) => (
+            <p key={index} className="text-xs text-red-400">
+              {message}
+            </p>
+          ))}
         </div>
 
         <div className="my-3">
           <Button>저장하기</Button>
         </div>
       </form>
-      <DebugStates fieldValues={fieldValues} />
+      <DebugStates
+        saveErrorMessages={saveErrorMessages}
+        fieldValues={fieldValues}
+      />
     </div>
   );
 }
-
 export default ArticleForm;
